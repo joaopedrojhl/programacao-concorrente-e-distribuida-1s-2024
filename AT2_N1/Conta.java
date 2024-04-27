e1246c5a3128ccb6f21bd58b447bfcd813525ac2
joaopedrojhl <jhlhoaopedro@gmail.com>
2024-04-26 22:57:15 -0300

    conta

---
+++ AT2_N1/Conta.java
@@ -0,0 +1,40 @@
+package AT2_N1;
+
+public class Conta {
+    private String numero;
+    private double saldo;
+
+    public Conta(String numero) {
+        this.numero = numero;
+        this.saldo = 0.0;
+    }
+
+    public Conta(String numero, double saldoInicial) {
+        this.numero = numero;
+        this.saldo = saldoInicial;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double novoSaldo) {
+        this.saldo = novoSaldo;
+    }    
+
+    public void depositar(double valor) {
+        saldo += valor;
+    }
+
+    public void sacar(double valor) {
+        if (saldo >= valor) {
+            saldo -= valor;
+        } else {
+            System.out.println("Saldo insuficiente para sacar R$" + valor);
+        }
+    }
+}
